40d6e446de8086765ab09b7fcff4b35ef816fe43
RadelNigma <radelnigma@yandex.ru>
2022-02-15 02:25:37 +0300

    Настройка соединения в Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -11,22 +11,55 @@ import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-   private static String dbURL = "jdbc:h2:mem:test; INIT=RUNSCRIPT FROM 'classpath:init.sql'";
-   private static String dbUserName = "sa";
-   private static String dbPassword = "";
 
-    public static Connection getConnection()  {
+    private static final String URL = "jdbc:mysql://localhost:3306/kata_radel";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "FFpe43FFpe43";
 
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(dbURL,dbUserName,dbPassword);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    private static Connection connection;
+
+    public static Connection getConnection() {
         return connection;
     }
 
+    static {
+        try {
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+  //  Connection DriverManager.getConnection(jdbc:mysql://127.0.0.1:3306)
+
+
+//        try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306","root","FFpe43FFpe43")){
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+
+
 }
+
+//    private static String dbURL = "jdbc:h2:mem:test; INIT=RUNSCRIPT FROM 'classpath:init.sql'";
+//    private static String dbUserName = "sa";
+//    private static String dbPassword = "";
+//
+//    public static Connection getConnection()  {
+//
+//        Connection connection = null;
+//        try {
+//            connection = DriverManager.getConnection(dbURL,dbUserName,dbPassword);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return connection;
+//    }
+
+
 
 //    String dbURL = null;
 //    String dbUserName = "sa";
