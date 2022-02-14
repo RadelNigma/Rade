222d5eaa36f30b9d446ffec800ef200327763f80
RadelNigma <radelnigma@yandex.ru>
2022-02-15 02:28:23 +0300

    getAllUsers() in UserDao

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,12 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -18,15 +23,50 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            String SQL = "INSERT INTO Users VALUES(" + 2 + ",'" + name + "','"
+                    + lastName + "'," + age + ")";
+            statement.executeUpdate(SQL);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void removeUserById(long id) {
 
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            String SQL = "";
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            String SQL = "SELECT * FROM Users";
+            ResultSet resultSet =  statement.executeQuery(SQL);
+
+            while (resultSet.next()){
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setName(resultSet.getString("lastName"));
+                user.setAge((byte) resultSet.getInt("age"));
+
+                userList.add(user);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
