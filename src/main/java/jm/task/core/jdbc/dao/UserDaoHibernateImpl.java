b39406f5582becd74c8831dbc3fb15a8361602be
RadelNigma <radelnigma@yandex.ru>
2022-09-04 02:20:59 +0300

    Final

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -23,6 +23,7 @@ public class UserDaoHibernateImpl implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
+            e.printStackTrace();
         }
     }
 
@@ -53,6 +54,7 @@ public class UserDaoHibernateImpl implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
+            e.printStackTrace();
         }
     }
 
@@ -61,22 +63,32 @@ public class UserDaoHibernateImpl implements UserDao {
         Transaction transaction = null;
         try (Session session = getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            session.remove(session.get(User.class, id));
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.remove(user);
+            }
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
+            e.printStackTrace();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> list = null;
+        Transaction transaction = null;
         try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
             list = session.createQuery("SELECT a FROM User a", User.class).list();
             System.out.println(list);
+            transaction.commit();
         } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             e.printStackTrace();
         }
         return list;
